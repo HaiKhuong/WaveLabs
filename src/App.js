c7847e6f3baf3db49422524e956eb5de0b0cdc71
HAI-KHUONG\Khuong <khuong.nguyen@itbeesolutions.vn>
2021-08-10 12:14:49 +0700

    Mobile Preview Application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainLayout from './layout/MainLayout';
+import Home from './page/home/home';
+import WhatNew from './page/whatNew/whatNew';
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import HeaderLayout from './layout/LayoutHeader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <HeaderLayout />
+      <MainLayout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/what-new" component={WhatNew} />
+          <Route
+            path="*"
+            name="Not Found"
+            render={() => <a>Page Not Found</a>}
+          />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
